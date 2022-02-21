9cad8c34b25b44dfabed86a6f51c519d4646759d
Jaziel Perez <pjaziel723@gmail.com>
2022-02-21 01:03:57 -0600

    PHP pruebas

---
+++ variable.php
@@ -0,0 +1,17 @@
+<!-- una constante siempre va a mantener el mismo valor -->
+
+<!-- En una variable de PHP siempre se inciara con signo de dolar -->
+<?php
+
+$numero_1 = 8;
+$numero_2 = 100;
+
+define("NUMERO_PI", 3.14);
+// DEFINE ("NUMERO_PI", 14);
+ECHO NUMERO_PI . 2;
+
+
+echo"\n";
+
+define("MSG", "His solo learn");
+echo MSG;
