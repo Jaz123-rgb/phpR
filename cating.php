7e2f53f54702f1cca3a74b199becad07f38ff64a
Jaziel Perez <pjaziel723@gmail.com>
2022-02-21 11:40:31 -0600

    se agrego el archvo tipos.php

---
+++ cating.php
@@ -0,0 +1,26 @@
+<?php
+$numerito = "5";
+
+var_dump($numerito);
+$numerito = (int) $numerito;
+
+var_dump($numerito);
+
+
+
+$dias = 5.89;
+var_dump($dias);   // cating es la forma de convertir un tipo de datoa otro dato 
+$dias = (int) $dias;
+
+var_dump($dias);
+
+
+$banderitas = false;
+var_dump($banderitas);
+$banderitas = (bool) $banderitas ;
+var_dump($banderitas);
+echo"\n";
+
+
+
+
