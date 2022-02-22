3e870db54048abe4759b638ccb0f55c3977b3eb8
Jaziel Perez <pjaziel723@gmail.com>
2022-02-22 01:27:29 -0600

    Se agrego el archivo de precedencia

---
+++ precedencia.php
@@ -0,0 +1,18 @@
+<?php
+$contador = 1;
+$resultado = $contador++;
+
+echo $resultado . '<br>';
+
+//TIENE ASOCIATIVIADA DE IZQUIERDA
+
+echo 1- (2-3) . '<br>';
+
+//TIENE UNA AOSIACTIVIDAD DE DERECHA 
+ 
+$a = true;
+$b = false;
+ $resultado = ($a and $b);
+ var_dump($resultado);
+
+
