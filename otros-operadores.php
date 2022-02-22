14e7af9d32167a90c13e86eb00aeccde8e110b17
Jaziel Perez <pjaziel723@gmail.com>
2022-02-22 01:06:18 -0600

    Se agrego el archivo otros opreadores

---
+++ otros-operadores.php
@@ -0,0 +1,37 @@
+<?php
+
+//OPERADOR DE ASIGNACION =
+ 
+$edad1 = ($edad2 = 10)+ 18;
+echo "la suma de dos edades es: $edad1 <br>";
+echo "la edad dentro de la suma es $edad2 <br>";
+
+// OPERADORES DE INCREMENTO 
+$contador = 1;
+$contador = $contador + 1;
+echo $contador . "<br>";
+//OTRA FORMA
+ $cont = 2;
+$cont += 1;
+echo $cont . "<br>";
+
+//Otra forma mas corta 
+$cont1 = 1;
+$cont1 ++;
+echo $cont1 . "<br>";
+/*
+  como esite el operadoer tambien esta el 
+  +=
+  -=
+  *=
+  /=
+*/
+$nomre = "Carlos";
+ $nomre .= " " . "Perez";
+ echo $nomre . "<br>";
+
+
+ 
+
+
+
