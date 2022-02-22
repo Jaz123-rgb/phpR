67bb2167dc079346f89185e5fcaf575c8fc82493
Jaziel Perez <pjaziel723@gmail.com>
2022-02-21 23:18:32 -0600

    Se agrego el archivo operadores aritemeticos

---
+++ operadores_aritmeticos.php
@@ -0,0 +1,43 @@
+<?php
+
+$resultado =54 - 21;
+
+echo $resultado;
+
+
+?>
+
+<br>
+
+
+<?php
+$resultado = 5**21;
+
+echo $resultado;
+
+
+?>
+<br>
+
+<?php
+
+echo("estas es una prueba \n");
+echo(5*5) ." ".(5*5);
+
+?>
+<br>
+
+<?php
+
+echo 5 / 2 . "<br>";
+echo 5 % 2 . "<br>";
+
+$horas = 7501;
+$segundos = 3600;
+
+echo "$horas segndos converitidos a horas son " . ($horas / $segundos) . "<br>";
+echo "sobran " . ($horas % $segundos) . " segundos";
+
+?>
+
+<br>
